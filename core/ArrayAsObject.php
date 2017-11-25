a363209f88db1db4d6886247f9ef396b512f3461
mono <monotom@gmail.com>
2017-11-25 23:52:17 +0100

    formatting

--- core/ArrayAsObject.php
+++ core/ArrayAsObject.php
@@ -4,6 +4,7 @@ namespace cw\php\core;
 
 class ArrayAsObject extends \ArrayObject{
   public function __construct($input = []){
-    parent::__construct($input, \ArrayObject::STD_PROP_LIST | \ArrayObject::ARRAY_AS_PROPS);
+    parent::__construct($input, \ArrayObject::STD_PROP_LIST |
+                                \ArrayObject::ARRAY_AS_PROPS);
   }
 }
