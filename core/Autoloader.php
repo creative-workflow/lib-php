762d143fd1eab7d1a79f9e4d7c0aa50a43223ac8
Tom Hanoldt <tom@creative-workflow.berlin>
2017-12-02 14:57:36 +0100

    add cw bower autloader

--- core/Autoloader.php
+++ core/Autoloader.php
@@ -17,6 +17,28 @@ class Autoloader{
     });
   }
 
+
+  public static function registerCwBowerNamespaceLoader(){
+    $paths = func_get_args();
+
+    spl_autoload_register(function ($className) use($paths){
+      if(strpos($className, 'cw') === false)
+        return ;
+
+      $fileName = str_replace('\\', '/', $className);
+      $fileName = str_replace('cw/wp', 'cw/wordpress', $fileName);
+      $fileName = str_replace('cw/', 'creative-workflow.', $fileName).'.php';
+
+      foreach($paths as $path){
+        $filePath = $path . '/' . $fileName;
+
+        if(file_exists($filePath))
+          include $filePath;
+      }
+    });
+  }
+
+
   public static function registerSnakecaseLoader(){
     $paths = func_get_args();
 
