c74e8057543438d5c6e395198bfe303907f113e5
Tom Hanoldt <tom@creative-workflow.berlin>
2018-01-26 14:45:36 +0100

    add append method

--- js/expression/Wrapper.php
+++ js/expression/Wrapper.php
@@ -4,8 +4,13 @@ namespace cw\php\js\expression;
 
 class Wrapper extends AbstractExpression{
   public $js;
-  public function __construct($js){
-    $this->js = $js;
+  public function __construct($js=''){
+    $this->js = "$js";
+  }
+
+  public function append($input){
+    $this->js .= "$input";
+    return $this;
   }
 
   public function toJs(){
