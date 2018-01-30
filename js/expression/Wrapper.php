2dd8b0ad6d5fbea76f2807b1a8fbf486411f72f8
mono <monotom@gmail.com>
2018-01-30 15:00:28 +0100

    momorize js expression instead of casting to string instantly, cast happens when toJs gets called

--- js/expression/Wrapper.php
+++ js/expression/Wrapper.php
@@ -3,17 +3,18 @@
 namespace cw\php\js\expression;
 
 class Wrapper extends AbstractExpression{
-  public $js;
+  public $js = [];
   public function __construct($js=''){
-    $this->js = "$js";
+    $this->js[]= $js;
   }
 
   public function append($input){
-    $this->js .= "$input";
+    $this->js[]= $input;
     return $this;
   }
 
   public function toJs(){
-    return "<script type='text/javascript'>\n /* <![CDATA[ */ \n $this->js $this->trailingSemicolon \n /* ]]> */</script>";
+    $js = implode(';', $this->js);
+    return "<script type='text/javascript'>\n /* <![CDATA[ */ \n $js $this->trailingSemicolon \n /* ]]> */</script>";
   }
 }
