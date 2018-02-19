bb9097b08a0904f2426eef3af3fa1504c8b3aa4b
mono <monotom@gmail.com>
2018-02-19 22:34:31 +0100

    add toArray and implement ArrayAccess

--- core/ArrayAsObject.php
+++ core/ArrayAsObject.php
@@ -2,9 +2,13 @@
 
 namespace cw\php\core;
 
-class ArrayAsObject extends \ArrayObject{
+class ArrayAsObject extends \ArrayObject implements \ArrayAccess{
   public function __construct($input = []){
     parent::__construct($input, \ArrayObject::STD_PROP_LIST |
                                 \ArrayObject::ARRAY_AS_PROPS);
+  }
+
+  public function toArray(){
+    return $this->getArrayCopy();
   }
 }
--- core/ObjectAsArray.php
+++ core/ObjectAsArray.php
@@ -2,12 +2,16 @@
 
 namespace cw\php\core;
 
-class ObjectAsArray extends \ArrayObject{
+class ObjectAsArray extends \ArrayObject implements \ArrayAccess{
   public function __construct($input = []){
     // convert nested objects to array
     $input = json_decode(json_encode($input), true);
 
     parent::__construct($input, \ArrayObject::STD_PROP_LIST |
                                 \ArrayObject::ARRAY_AS_PROPS);
+  }
+
+  public function toArray(){
+    return $this->getArrayCopy();
   }
 }
